be909fd8d838487b6bd03aa84eca69c828f69e77
Zyn0rX ツ <jorgemontanez272011@gmail.com>
2025-12-06 23:33:24 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, query, where, orderBy } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
+// --- ⚠️ PEGA AQUÍ TUS CLAVES DE FIREBASE ⚠️ ---
 const firebaseConfig = {
   apiKey: "AIzaSyAsw_4hr5GQPKjjevEzIz9XMIMxqmuBe84",
   authDomain: "mdt-nacional.firebaseapp.com",
@@ -16,5 +14,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Exportamos para que los otros archivos puedan usarlo
-export { db, collection, addDoc, getDocs, query, where };
+// Exportamos las herramientas
+export { db, collection, addDoc, getDocs, query, where, orderBy };
